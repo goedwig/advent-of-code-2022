babd43f1e311d12d1d104b41ccc34b602845501b
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2022-12-04 10:06:43 +0200

    Add day 4 puzzles

---
+++ 4/1.py
@@ -0,0 +1,16 @@
+f = open("4/input.txt")
+
+full_containment = 0
+
+for line in f:
+    line = line.rstrip()
+    bounds_1, bounds_2 = line.split(",")
+    bounds_1 = tuple(map(int, bounds_1.split("-")))
+    bounds_2 = tuple(map(int, bounds_2.split("-")))
+
+    if bounds_1[0] >= bounds_2[0] and bounds_1[1] <= bounds_2[1]:
+        full_containment += 1
+    elif bounds_2[0] >= bounds_1[0] and bounds_2[1] <= bounds_1[1]:
+        full_containment += 1
+
+print(full_containment)
---
+++ 4/2.py
@@ -0,0 +1,17 @@
+f = open("4/input.txt")
+
+overlap = 0
+
+for line in f:
+    line = line.rstrip()
+    bounds_1, bounds_2 = line.split(",")
+    bounds_1 = tuple(map(int, bounds_1.split("-")))
+    bounds_2 = tuple(map(int, bounds_2.split("-")))
+
+    range_1 = range(bounds_1[0], bounds_1[1] + 1)
+    range_2 = range(bounds_2[0], bounds_2[1] + 1)
+
+    if set(range_1) & set(range_2):
+        overlap += 1
+
+print(overlap)
