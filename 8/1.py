3d12ca8c3d03a17960b2d94848647b1bd6a9df13
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2022-12-08 09:47:28 +0200

    Add day 8 puzzles

---
+++ 8/1.py
@@ -0,0 +1,37 @@
+with open("8/input.txt") as f:
+    tree_map = [list(map(int, line)) for line in f.read().splitlines()]
+
+visible = len(tree_map) * 4 - 4
+
+for i in range(1, len(tree_map) - 1):
+    for j in range(1, len(tree_map[i]) - 1):
+        tree_height = tree_map[i][j]
+        is_visible = False
+
+        for k in range(i - 1, -1, -1):
+            if tree_height <= tree_map[k][j]:
+                break
+        else:
+            is_visible = True
+
+        for k in range(i + 1, len(tree_map), 1):
+            if tree_height <= tree_map[k][j]:
+                break
+        else:
+            is_visible = True
+
+        for k in range(j - 1, -1, -1):
+            if tree_height <= tree_map[i][k]:
+                break
+        else:
+            is_visible = True
+
+        for k in range(j + 1, len(tree_map[i]), 1):
+            if tree_height <= tree_map[i][k]:
+                break
+        else:
+            is_visible = True
+
+        visible += is_visible
+
+print(visible)
---
+++ 8/2.py
@@ -0,0 +1,35 @@
+from functools import reduce
+
+with open("8/input.txt") as f:
+    tree_map = [list(map(int, line)) for line in f.read().splitlines()]
+
+best_scenic_score = 0
+
+for i in range(len(tree_map)):
+    for j in range(len(tree_map[i])):
+        tree_height = tree_map[i][j]
+        viewing_distances = [0, 0, 0, 0]
+
+        for k in range(i - 1, -1, -1):
+            viewing_distances[0] += 1
+            if tree_map[k][j] >= tree_height:
+                break
+
+        for k in range(i + 1, len(tree_map), 1):
+            viewing_distances[1] += 1
+            if tree_map[k][j] >= tree_height:
+                break
+
+        for k in range(j - 1, -1, -1):
+            viewing_distances[2] += 1
+            if tree_map[i][k] >= tree_height:
+                break
+
+        for k in range(j + 1, len(tree_map[i]), 1):
+            viewing_distances[3] += 1
+            if tree_map[i][k] >= tree_height:
+                break
+
+        best_scenic_score = max(best_scenic_score, reduce(lambda a, b: a * b, viewing_distances))
+
+print(best_scenic_score)
