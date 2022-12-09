1953eeaba787691a134383f5f17107023bf82254
Alexander Tarasov <37976075+goedwig@users.noreply.github.com>
2022-12-09 20:31:31 +0200

    Add day 9 puzzles

---
+++ 9/1.py
@@ -0,0 +1,41 @@
+with open("9/input.txt") as f:
+    moves = [(line[0], int(line[2:])) for line in f.read().splitlines()]
+
+Hx, Hy = 0, 0
+Tx, Ty = 0, 0
+
+T_visited = {(0, 0)}
+
+
+def inc(x, y, direction):
+    match direction:
+        case "U":
+            return x, y + 1
+        case "D":
+            return x, y - 1
+        case "L":
+            return x - 1, y
+        case "R":
+            return x + 1, y
+
+for direction, count in moves:
+    for _ in range(count):
+        Hx, Hy = inc(Hx, Hy, direction)
+
+        if abs(Hx - Tx) <= 1 and abs(Hy - Ty) <= 1:
+            continue
+
+        if Hx == Tx or Hy == Ty:
+            deltas = ((0, 1), (0, -1), (0, 1), (0, -1), (1, 0), (-1, 0))
+        else:
+            deltas = ((1, 1), (1, -1), (-1, 1), (-1, -1))
+
+        for dx, dy in deltas:
+            Tx_new, Ty_new = Tx + dx, Ty + dy
+            if abs(Hx - Tx_new) <= 1 and abs(Hy - Ty_new) <= 1:
+                Tx = Tx_new
+                Ty = Ty_new
+                T_visited.add((Tx, Ty))
+                break
+
+print(len(T_visited))
---
+++ 9/2.py
@@ -0,0 +1,47 @@
+with open("9/input.txt") as f:
+    moves = [(line[0], int(line[2:])) for line in f.read().splitlines()]
+
+Hx, Hy = 0, 0
+Tx = [0] * 9
+Ty = [0] * 9
+
+T_visited = {(0, 0)}
+
+def inc(x, y, direction):
+    match direction:
+        case "U":
+            return x, y + 1
+        case "D":
+            return x, y - 1
+        case "L":
+            return x - 1, y
+        case "R":
+            return x + 1, y
+
+
+for direction, count in moves:
+    for _ in range(count):
+        Hx, Hy = inc(Hx, Hy, direction)
+
+        hx, hy = Hx, Hy
+        for k in range(9):
+            tx, ty = Tx[k], Ty[k]
+
+            if abs(hx - tx) <= 1 and abs(hy - ty) <= 1:
+                break
+
+            if hx == tx or hy == ty:
+                deltas = ((0, 1), (0, -1), (1, 0), (-1, 0))
+            else:
+                deltas = ((1, 1), (1, -1), (-1, 1), (-1, -1))
+
+            for dx, dy in deltas:
+                tx_new, ty_new = tx + dx, ty + dy
+                if abs(hx - tx_new) <= 1 and abs(hy - ty_new) <= 1:
+                    Tx[k], Ty[k] = tx_new, ty_new
+
+            hx, hy = Tx[k], Ty[k]
+
+        T_visited.add((Tx[-1], Ty[-1]))
+
+print(len(T_visited))
